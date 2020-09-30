1925568020ce05698e35f86eb6e578a8093bd217
廣瀬健二朗 <hiroken506@gmail.com>
2020-09-30 17:05:34 +0900

    playerの基本操作の追加

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    //Rigidbody rb;
+    public float jumppower = 0.5f;
+    public float speed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            transform.position += new Vector3(0,-jumppower,0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+
+            transform.position += new Vector3(0, jumppower, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            transform.position += new Vector3(-speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            transform.position += new Vector3(speed, 0, 0);
+        }
+    }
+}
