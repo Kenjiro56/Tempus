ae08e31aec61eeb84c79501c1ded7559b4ea4b44
廣瀬健二朗 <hiroken506@gmail.com>
2020-10-02 23:47:37 +0900

    cameraの設定2

---
+++ Assets/Scripts/camerascript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerascript : MonoBehaviour
+{
+    public GameObject chara;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!charaScript.reve) {
+            this.transform.parent = chara.transform;
+
+        }
+    }
+}
