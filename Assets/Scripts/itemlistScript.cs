07b95913c30daeebde11c5bf850cf1da00356312
廣瀬健二朗 <hiroken506@gmail.com>
2020-10-17 12:36:08 +0900

    ItemList画面の編集(アイテム説明の追加)

---
+++ Assets/Scripts/itemlistScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class itemlistScript : MonoBehaviour
+{
+    public GameObject image;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        image.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        
+
+        image.SetActive(true);
+
+    }
+    private void OnMouseExit()
+    {
+        image.SetActive(false);
+    }
+}
